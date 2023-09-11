01dec9c990f74f6e34a40594ed9129e4eb3894e2
BCWagnon <bridget.wagnon4552@gmail.com>
2023-09-11 18:37:04 -0500

    One finished card with styling

--- expanding-cards/src/App.js
+++ expanding-cards/src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import {motion} from 'framer-motion';
+import {useState} from 'react';
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <motion.div  
+      transition={{layout: {duration: 1, type: "spring" }}} 
+      layout onClick={() => setIsOpen(!isOpen)} 
+      className='card'
+      style={{
+        borderRadius: "1rem",
+        boxShadow: "0px 10px 30px rgba(0,0,0, 0.5)"
+      }}
+      >
+        <motion.h2 layout="position">Ellie</motion.h2>
+        {isOpen && (
+          <motion.div 
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1 }}
+          className='expand'
+          >
+            <div>
+              <p>Name: Ellie</p>
+              <p>Age: 5 years old</p>
+              <p>Breed: 1/4 Newfoundlad, 1/4 St.Bernard, 1/4 Great Pyreneese, and 1/4 Greater Swiss Mountain Dog</p>
+              <p>Personality: Smelly Ellie is the first animal my partner and I added to our family. She will always have a specail place in our hearts. Thant being said- shes stinky! Shes goofy and very playful but is also VERY shy.</p>
+              <p>Favorites:
+                <br/>
+              Favorite Toy: Her Hedgehog
+              <br/>
+              Favorite Treats: Chicken Nugget- if chicken nuggets arent available she likes any meat flavored treats
+              <br/>
+              Favorite Spot: Anywhere that is in the way
+              </p>
+              <p>Health: Ellie gets an upset stomach very easily which can lead her to throwing up. She also has a limp especially when first getting up- it goes away after moving a bit.</p>
+            </div>
+          </motion.div>
+        )}
+      </motion.div>
+      
     </div>
   );
 }
